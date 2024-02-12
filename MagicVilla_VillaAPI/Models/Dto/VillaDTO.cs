7af93b87be85c791d3081b8ee1c12867013bbc01
Thomas Ngo <tngo0508@gmail.com>
2024-02-12 12:51:37 -0800

    use EF to create table

--- MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -7,9 +7,13 @@ namespace MagicVilla_VillaAPI.Models.Dto
         public int Id { get; set; }
         [Required]
         [MaxLength(30)]
+        public string Details { get; set; }
+        [Required]
         public string Name { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public int Occupancy { get; set; }
+        public double Rate { get; set; }
         public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
     }
 }
